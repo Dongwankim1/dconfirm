727d41041153db952fc428c0797bedcfd786fa3f
dongwan <zkdlwu94@naver.com>
2022-01-23 23:04:37 +0900

    first commit

---
+++ dconfirm/dconfirm.js
@@ -0,0 +1,29 @@
+/**
+ * Bead Sort, also known as Gravity sort.
+ *
+ * This algorithm was inspired from natural phenomena and was designed keeping in mind objects (or beads) falling under
+ * the influence of gravity.
+ *
+ * NOTE: It only works for arrays of positive integers.
+ *
+ * 
+ */
+
+
+/**
+ * 
+ * @param {html} elem  html structure to use.
+ * @param {string} content  Display the contents on the screen.
+ * @param {string} innerElem 
+ * @param {function} callback 
+ * @returns {boolean} 
+ */
+
+function Dconfirm(elem,innerElem,content,callback){
+    this.elem = elem;
+    this.innerElem = innerElem;
+    this.content = content;
+    this.callback = callback;
+
+}
+
