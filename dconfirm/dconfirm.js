95862ba48b9a829926696bb33c2c3901c619f324
dongwan <zkdlwu94@naver.com>
2022-01-23 23:16:25 +0900

    ..

--- dconfirm/dconfirm.js
+++ dconfirm/dconfirm.js
@@ -12,18 +12,32 @@
 
 /**
  * 
- * @param {html} elem  html structure to use.
+ * @param {html} mainElem  html structure to use.
+ * @param {string} innerElemId 
+ * @param {Object} options {width : window width size, height : window height size }
  * @param {string} content  Display the contents on the screen.
- * @param {string} innerElem 
  * @param {function} callback 
  * @returns {boolean} 
  */
 
-function Dconfirm(elem,innerElem,content,callback){
-    this.elem = elem;
-    this.innerElem = innerElem;
+function Dconfirm(mainElem,innerElemId,options,content,callback){
+    this.mainElem = mainElem;
+    this.innerElemId = innerElemId;
+    this.options = options;
     this.content = content;
     this.callback = callback;
-
+    this.create();
 }
 
+Dconfirm.prototype.create = function(){
+    this.mainElem.style.display = "none";
+    document.body.appendChild(this.mainElem);
+}
+
+Dconfirm.prototype.confirm = function(){
+    
+}
+
+Dconfirm.prototype.alert = function(){
+    
+}
